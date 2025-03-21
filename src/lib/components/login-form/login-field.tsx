355c53a529af3aa6621fc3c20b6949adc478452c
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-21 22:56:37 +0000

    internal login field component for login form.

--- src/lib/components/login-form/login-field.tsx
+++ src/lib/components/login-form/login-field.tsx
@@ -1,12 +1,14 @@
-import { createMemo } from "solid-js";
+import { Show, createMemo, createSignal } from "solid-js";
 import { getSprite } from "~/lib/common/resources";
 import styles from "./login-field.module.css";
 
 interface Props {
 	fieldType: "email" | "password";
+	onChange: (value: string) => void;
 }
 
 export default function LoginField(props: Props) {
+	let inputElement: HTMLInputElement | undefined;
 	const renderProps = createMemo(() => {
 		if (props.fieldType === "email") {
 			return {
@@ -22,6 +24,17 @@ export default function LoginField(props: Props) {
 		};
 	});
 
+	const [isPasswordHidden, setPasswordHidden] = createSignal(false);
+	const inputType = createMemo(() => {
+		if (props.fieldType === "email") {
+			return "email";
+		}
+		if (isPasswordHidden()) {
+			return "password";
+		}
+		return "text";
+	});
+
 	return (
 		<div class={`${styles.container} row_centred`}>
 			<svg>
@@ -29,9 +42,24 @@ export default function LoginField(props: Props) {
 				<use href={renderProps().icon} />
 			</svg>
 			<input
+				ref={inputElement}
 				placeholder={renderProps().placeholder}
-				type={renderProps().type}
+				type={inputType()}
+				on:change={(e) => props.onChange(e.target.value)}
 			/>
+			<Show when={renderProps().type === "password"}>
+				<button
+					class="button_icon"
+					on:click={() => setPasswordHidden((prev) => !prev)}
+					type="button"
+					aria-label={isPasswordHidden() ? "Show password" : "Hide password"}
+				>
+					<svg>
+						<title>{isPasswordHidden() ? "Eye icon" : "Eye-off icon"}</title>
+						<use href={getSprite(isPasswordHidden() ? "eye" : "eye-off")} />
+					</svg>
+				</button>
+			</Show>
 		</div>
 	);
 }
